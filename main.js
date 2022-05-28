e3b3b018843f9f5d8521327e91089334882d912c
RustyNox <noelmdixon@gmail.com>
2022-05-28 17:39:37 +1000

    Added static handling for public css files

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.use(express.static('./public'));
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Title is passed from the server!' });
+    res.render('home', { title: 'Title here' });
 });
 
 app.listen(port, () => {
